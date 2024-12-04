7023bb796b0f7b445f0c1e2f8d4c77e2e8e548e3
Thomas Hasselgreen <thasselgreen@gmail.com>
2024-12-04 16:44:25 +0100

    fixed part 1/2

--- day3/part1.py
+++ day3/part1.py
@@ -1,65 +1,53 @@
 from os import system
 
-def mul(contents, enabled):
+def mul(contents):
     total = 0
     
     while "mul" in contents: 
-        while enabled and "mul" in contents: 
-            location = contents.find("mul")
-            disableLocation = contents.find("don't()")
+        location = contents.find("mul")
 
-            if disableLocation < location:
-                enabled = False
-                continue
+        num1, num2 = "0", "0"
 
-            num1, num2 = "0", "0"
+        finding1 = True
 
-            finding1 = True
+        found = True
+        rangeEnd = location + 3
 
-            found = True
-            rangeEnd = location + 3
+        if contents[location + 3] == "(":
+            i = location + 4
 
-            if contents[location + 3] == "(":
-                i = location + 4
+            while contents[i] != ")":
+                try:
+                    int(contents[i])
 
-                while contents[i] != ")":
-                    try:
-                        int(contents[i])
+                    if finding1: 
+                        num1 += str(contents[i])
 
-                        if finding1: 
-                            num1 += str(contents[i])
+                    else:
+                        num2 += str(contents[i])
 
-                        else:
-                            num2 += str(contents[i])
+                except ValueError:
+                    if contents[i] == ",":
+                        finding1 = False
+                    else:
+                        found = False
+                        break
 
-                    except ValueError:
-                        if contents[i] == ",":
-                            finding1 = False
-                        else:
-                            found = False
-                            break
+                i += 1
 
-                    i += 1
+            rangeEnd = i
 
-                rangeEnd = i
+        else: 
+            found = False
 
-            else: 
-                found = False
+        contents = contents[rangeEnd:]
 
-            contents = contents[rangeEnd:]
+        if found:
+            total += int(num1) * int(num2)
+    
+    return total
 
-            if found:
-                total += int(num1) * int(num2)
-        
-        while not enabled and "do()" in contents:
-            newLocation = contents.find("do()")
-            enabled = True
-            
-            contents = contents[newLocation:]
-
-    return total, enabled
-
-with open("day3/testInput.txt", "r") as inputText:
+with open("day3/input.txt", "r") as inputText:
     system("cls")
 
     contentsInLines = inputText.readlines()
@@ -69,9 +57,7 @@ with open("day3/testInput.txt", "r") as inputText:
     result = (0, True)
 
     for i in contentsInLines:
-        result = mul(i, result[1])
-
-        total += result[0]
+        total += mul(i)
 
     with open("day3/outputs.txt", "r") as earlier:
         numbers = [int(line.strip()) for line in earlier.readlines()]
---
+++ day3/part2.py
@@ -0,0 +1,85 @@
+from os import system
+
+def mul(contents, enabled):
+    total = 0
+    
+    while "mul" in contents: 
+        while enabled and "mul" in contents: 
+            location = contents.find("mul")
+            disableLocation = contents.find("don't()")
+
+            if disableLocation < location:
+                enabled = False
+                continue
+
+            num1, num2 = "0", "0"
+
+            finding1 = True
+
+            found = True
+            rangeEnd = location + 3
+
+            if contents[location + 3] == "(":
+                i = location + 4
+
+                while contents[i] != ")":
+                    try:
+                        int(contents[i])
+
+                        if finding1: 
+                            num1 += str(contents[i])
+
+                        else:
+                            num2 += str(contents[i])
+
+                    except ValueError:
+                        if contents[i] == ",":
+                            finding1 = False
+                        else:
+                            found = False
+                            break
+
+                    i += 1
+
+                rangeEnd = i
+
+            else: 
+                found = False
+
+            contents = contents[rangeEnd:]
+
+            if found:
+                total += int(num1) * int(num2)
+        
+        while not enabled and "do()" in contents:
+            newLocation = contents.find("do()")
+            enabled = True
+            
+            contents = contents[newLocation:]
+
+    return total, enabled
+
+with open("day3/input.txt", "r") as inputText:
+    system("cls")
+
+    contentsInLines = inputText.readlines()
+
+    total = 0
+
+    result = (0, True)
+
+    for i in contentsInLines:
+        result = mul(i, result[1])
+
+        total += result[0]
+
+    with open("day3/outputs.txt", "r") as earlier:
+        numbers = [int(line.strip()) for line in earlier.readlines()]
+
+    with open("day3/outputs.txt", "a") as earlier:
+
+        if total in numbers:
+            print(f"{total}, already seen")
+        else:
+            print(total)
+            earlier.write(str(total))
