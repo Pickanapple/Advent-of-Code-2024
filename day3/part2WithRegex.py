57a13e56d2547b7fa41892f42cffc4c38afac7de
pickanapple <thasselgreen@gmail.com>
2024-12-05 10:22:55 +0100

    started work on part 2 with regex

---
+++ day3/part2WithRegex.py
@@ -0,0 +1,42 @@
+import re
+
+match = r"mul\(\d{1,3},\d{1,3}\)"
+
+def trimList(list, enabled):
+    while "do" in list:
+        if not enabled and "do()" in list:
+            enabled = True 
+            list = list[list.find("do()" + 4):]
+
+        if enabled and "don't()" in list:
+            if "do()" in list:
+                list = list[:list.find("don't()") + 1] + list[list.find("do()") + 1:]
+            else:
+                list = list[:list.find("don't()") + 1]
+                enabled = False
+
+    return list, enabled
+
+with open("day3/input.txt", "r") as file:
+    contents = file.readlines()
+
+    muls = []
+
+    total = 0
+
+    enabled = True
+
+    for i in contents: 
+        i, enabled = trimList(i, enabled)
+        muls.append(re.findall(match, i))
+
+    for i in muls:
+        for j in i:
+            j = j.replace("mul(", "")
+            j = j.replace(")", "")
+
+            commaIndex = j.index(",")
+
+            total += int(j[:commaIndex]) * int(j[commaIndex + 1:])
+
+    print(total)
