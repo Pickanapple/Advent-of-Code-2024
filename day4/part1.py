4c80126457c5cfe5ede22bed3baaee081ef41382
Thomas Hasselgreen <thasselgreen@gmail.com>
2024-12-05 07:16:25 +0100

    work on day 4

---
+++ day4/part1.py
@@ -0,0 +1,42 @@
+def nextCharacter(index, text):
+    global total
+
+    if index == []:
+        return
+
+    for i in index:
+        newIndex = []
+
+        if text[i[0]][i[1]] == "S":
+            total += 1
+            return
+
+        characterToFind = "XMAS"["XMAS".index(text[i[0]][i[1]]) + 1]
+
+        if text[i[0] + 1][i[0]] == characterToFind:
+            newIndex.append((i[0] + 1, i[0]))
+        
+        elif text[i[0]][i[0] + 1] == characterToFind:
+            newIndex.append((i[0], i[0] + 1))
+        
+        elif text[i[0] - 1][i[0]] == characterToFind:
+            newIndex.append((i[0] - 1, i[0]))
+        
+        elif text[i[0]][i[0] - 1] == characterToFind:
+            newIndex.append((i[0], i[0] - 1))
+
+        nextCharacter(newIndex, text)
+
+with open("day4/testInput.txt", "r") as inputText:
+    global total
+    total = 0
+
+    contents = inputText.readlines()
+
+    for i in range(len(contents)):
+        for j in range(len(contents)):
+            if contents[i][j] == "X":
+                nextCharacter([(i, j)], contents)
+
+
+    print(total)
