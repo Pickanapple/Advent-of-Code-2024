d8b954315c7fcd552d8449faa8e9874c37997b34
Thomas Hasselgreen <thasselgreen@gmail.com>
2024-12-09 18:17:11 +0100

    solved part 1

--- day9/part1.py
+++ day9/part1.py
@@ -1,7 +1,39 @@
 def readString(string):
-    for i in range(len(string)):
-        if 
+    list = []
+    id = 0
 
-with open("day9/testInput.txt", "r") as inputText:
-    contents = inputText.readline().removesuffix("\n")
+    for i in range(len(string)):
+        if i % 2 == 0:
+            for j in range(int(string[i])):
+                list.append(id)
+            
+            id += 1
+
+        
+        else:
+            for j in range(int(string[i])):
+                list.append(".")
+
+    return list
+
+def move(listToUse: list):
+    while "." in listToUse:
+        for i in range(len(listToUse) - 1, -1, -1):
+            if listToUse[i] != ".":
+                listToUse[listToUse.index(".")] = listToUse.pop(i)
+                break
     
+    return listToUse
+
+def calculate(listToUse: list):
+    total = 0
+
+    for i in range(len(listToUse)):
+        total += listToUse[i] * i
+
+    return total
+
+with open("day9/input.txt", "r") as inputText:
+    contents = inputText.readline().removesuffix("\n")
+
+    print(calculate(move(readString(contents))))
