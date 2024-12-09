982779b26a7168197d575463dbca81e0bd680dd9
pickanapple <thasselgreen@gmail.com>
2024-12-09 17:23:31 +0100

    started day 9

---
+++ day9/part1.py
@@ -0,0 +1,7 @@
+def readString(string):
+    for i in range(len(string)):
+        if 
+
+with open("day9/testInput.txt", "r") as inputText:
+    contents = inputText.readline().removesuffix("\n")
+    
