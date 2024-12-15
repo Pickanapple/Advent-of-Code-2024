a555158772c2afeee5c87f6087bcd7e21a52f530
Thomas Hasselgreen <thasselgreen@gmail.com>
2024-12-15 11:35:35 +0100

    solved day 10

--- day10/part2.py
+++ day10/part2.py
@@ -2,46 +2,39 @@ import sys
 sys.setrecursionlimit(2147483647)
 
 def search(listToCheck, coordinate):
-    global foundCoord
-
     currentNum = listToCheck[coordinate[0]][coordinate[1]]
     somethingHappened = False
 
     totalFound = 0
 
     if currentNum == 9:
-        if coordinate in foundCoord:
-            return 0, listToCheck
-        
-        foundCoord.append(coordinate)
-        return 1, listToCheck
+        return 1
 
     if coordinate[0] < len(listToCheck) - 1:
         if listToCheck[coordinate[0] + 1][coordinate[1]] == currentNum + 1: 
-            totalFound += search(listToCheck, (coordinate[0] + 1, coordinate[1]))[0]
+            totalFound += search(listToCheck, (coordinate[0] + 1, coordinate[1]))
             somethingHappened = True
 
     if coordinate[0] != 0:
         if listToCheck[coordinate[0] - 1][coordinate[1]] == currentNum + 1: 
-            totalFound += search(listToCheck, (coordinate[0] - 1, coordinate[1]))[0]
+            totalFound += search(listToCheck, (coordinate[0] - 1, coordinate[1]))
             somethingHappened = True
 
             
     if coordinate[1] < len(listToCheck[0]) - 1:
         if listToCheck[coordinate[0]][coordinate[1] + 1] == currentNum + 1: 
-            totalFound += search(listToCheck, (coordinate[0], coordinate[1] + 1))[0]
+            totalFound += search(listToCheck, (coordinate[0], coordinate[1] + 1))
             somethingHappened = True
             
     if coordinate[1] != 0:
         if listToCheck[coordinate[0]][coordinate[1] - 1] == currentNum + 1: 
-            totalFound += search(listToCheck, (coordinate[0], coordinate[1] - 1))[0]
+            totalFound += search(listToCheck, (coordinate[0], coordinate[1] - 1))
             somethingHappened = True
 
     if not somethingHappened:
-        listToCheck[coordinate[0]][coordinate[1]] = ""
-        return totalFound, 0
+        return totalFound
     
-    return totalFound, listToCheck
+    return totalFound
 
 def count2DListOccurences(listToCount, n): 
     total = 0
@@ -59,7 +52,7 @@ def len2DList(listToCheck):
 
     return lenOfList
 
-with open("day10/testInput.txt", "r") as inputText: 
+with open("day10/input.txt", "r") as inputText: 
     contents = [[int(i) for i in j.removesuffix("\n")] for j in inputText.readlines()]
 
     total = 0
@@ -67,11 +60,6 @@ with open("day10/testInput.txt", "r") as inputText:
     for i in range(len(contents)):
         for j in range(len(contents[i])):
             if contents[i][j] == 0:
-                foundCoord = []
-                augment, contents = search(contents, (i, j))
-                total += augment
+                total += search(contents, (i, j))
 
-# for i in contents:
-#     print(i)
-
-print(len2DList(contents) - count2DListOccurences(contents, ""))
+print(total)
